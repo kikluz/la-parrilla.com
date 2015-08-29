4f01857faf4e996d979b5932965396a5fc4a052d
kikluz <savir81@hotmail.com>
2015-08-29 15:57:38 -0700

    add new code for la parrilla

--- source/assets/javascripts/components/slideShow.js
+++ source/assets/javascripts/components/slideShow.js
@@ -1,144 +1,35 @@
-$(document).ready(function() {
+var counter = 0, // to keep track of current slide
+    $items = $('.hero-slideshow figure'), // a collection of all of the slides, caching for performance
+    numItems = $items.length; // total number of slides
 
-// ------------------------------------------------------------
-// slide show
-// ------------------------------------------------------------
+// this function is what cycles the slides, showing the next or previous slide and hiding all the others
+var showCurrent = function(){
+    var itemToShow = Math.abs(counter%numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show  
+   
+  $items.removeClass('show'); // remove .show from whichever element currently has it
+  $items.eq(itemToShow).addClass('show');    
+};
 
- // show first image no matter what
-$('#hero-overflow :last-child').show();
-
-setInterval(function() {
-
-  // take bottom image
-  var imgElement = $('#hero-overflow :first-child');
-
-  // hide it & append to top of the stack
-  imgElement.hide().appendTo('#hero-overflow');
-
-  // slowly fade in (2 seconds)
-  imgElement.fadeIn(2000);
-
-}, 4000);
+// add click events to prev & next buttons 
+$('.next').on('click', function(){
+    counter++;
+    showCurrent(); 
+});
+$('.prev').on('click', function(){
+    counter--;
+    showCurrent(); 
 });
 
-/*
-* EASYFADER - An Ultralight Fading Slideshow For Responsive Layouts
-* Version: 1.0
-* Author: Patrick Kunka
-* Copyright 2012-2013 Patrick Kunka
-*/
-
-(function($){
-	function prefix(el){
-		var prefixes = ["Webkit", "Moz", "O", "ms"];
-		for (var i = 0; i < prefixes.length; i++){
-			if (prefixes[i] + "Transition" in el.style){
-				return '-'+prefixes[i].toLowerCase()+'-'; 
-			};
-		}; 
-		return "transition" in el.style ? "" : false;
-	};
-	var methods = {
-		init: function(settings){
-			return this.each(function(){
-				var config = {
-					slideDur: 7000,
-					fadeDur: 800
-				};
-				if(settings){
-					$.extend(config, settings);
-				};
-				this.config = config;
-				var $container = $(this),
-					slideSelector = '.slide',
-					fading = false,
-					slideTimer,
-					activeSlide,
-					newSlide,
-					$slides = $container.find(slideSelector),
-					totalSlides = $slides.length,
-					$pagerList = $container.find('.pager_list');
-					prefix = prefix($container[0]);
-				function animateSlides(activeNdx, newNdx){
-					function cleanUp(){
-						$slides.eq(activeNdx).removeAttr('style');
-						activeSlide = newNdx;
-						fading = false;
-						waitForNext();
-					};
-					if(fading || activeNdx == newNdx){
-						return false;
-					};
-					fading = true;
-					$pagers.removeClass('active').eq(newSlide).addClass('active');
-					$slides.eq(activeNdx).css('z-index', 3);
-					$slides.eq(newNdx).css({
-						'z-index': 2,
-						'opacity': 1
-					});
-					if(!prefix){
-						$slides.eq(activeNdx).animate({'opacity': 0}, config.fadeDur,
-						function(){
-							cleanUp();
-						});
-					} else {
-						var styles = {};
-						styles[prefix+'transition'] = 'opacity '+config.fadeDur+'ms';
-						styles['opacity'] = 0;
-						$slides.eq(activeNdx).css(styles);
-						var fadeTimer = setTimeout(function(){
-							cleanUp();
-						},config.fadeDur);
-					};
-				};
-				function changeSlides(target){
-					if(target == 'next'){
-						newSlide = activeSlide + 1;
-						if(newSlide > totalSlides - 1){
-							newSlide = 0;
-						}
-					} else if(target == 'prev'){
-						newSlide = activeSlide - 1;
-						if(newSlide < 0){
-							newSlide = totalSlides - 1;
-						};
-					} else {
-						newSlide = target;
-					};
-					animateSlides(activeSlide, newSlide);
-				};
-				function waitForNext(){
-					slideTimer = setTimeout(function(){
-						changeSlides('next');
-					},config.slideDur);
-				};
-				for(var i = 0; i < totalSlides; i++){
-					$pagerList
-						.append('<li class="page" data-target="'+i+'">'+i+'</li>');
-				};
-				$container.find('.page').bind('click',function(){
-					var target = $(this).attr('data-target');
-					clearTimeout(slideTimer);
-					changeSlides(target);
-				});
-				var $pagers = $pagerList.find('.page');
-				$slides.eq(0).css('opacity', 1);
-				$pagers.eq(0).addClass('active');
-				activeSlide = 0;
-				waitForNext();
-			});
-		}
-	};
-	$.fn.easyFader = function(settings){
-		  return methods.init.apply(this, arguments);
-	};
-})(jQuery);
-
-$(function(){
-  $('#Fader').easyFader({
-    slideDur: 6000,
-    fadeDur: 800
+// if touch events are supported then add swipe interactions using TouchSwipe https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
+if('ontouchstart' in window){
+  $('.hero-slideshow').swipe({
+    swipeLeft:function() {
+      counter++;
+      showCurrent(); 
+    },
+    swipeRight:function() {
+      counter--;
+      showCurrent(); 
+    }
   });
-});
-
-
+}
