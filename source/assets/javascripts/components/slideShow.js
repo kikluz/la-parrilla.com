de5200e4aeec05e38299c09a7a926dff110023d0
kikluz <savir81@hotmail.com>
2015-11-14 15:19:33 -0800

    add new code fro modal

--- source/assets/javascripts/components/slideShow.js
+++ source/assets/javascripts/components/slideShow.js
@@ -37,32 +37,30 @@ $('.prev').on('click', function(){
   });
 }*/
 
-// multiples modals for menu images
+// this for gallery demo
 
-$('.open-modal').click(function() {
-  var el = $(this)[0],
-      classes = el.className.split(/\s+/);
-  for(var i = 0; i < classes.length; i++) {
-    if(classes[i].match(/modal-/)) {
-      var modalClass = classes[i];
-      $('.modal.' + modalClass).fadeIn('fast');
-    }
-  }
-});
+$(document).on('ready', function(){
+    $modal = $('.modal-frame');
+    $overlay = $('.modal-overlay');
 
-$('.close-modal').click(function() {
-  $(this).closest('.modal').fadeOut('fast');
-});
+    // Need this to clear out the keyframe classes so they dont clash with each other between ener/leave. Cheers. 
+    $modal.bind('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e){
+      if($modal.hasClass('state-leave')) {
+        $modal.removeClass('state-leave');
+      }
+    });
 
+    $('.close').on('click', function(){
+      $overlay.removeClass('state-show');
+      $modal.removeClass('state-appear').addClass('state-leave');
+    });
 
-$('button').click(function(e) {
-  $('.overlay, .modal').addClass('active');
-  e.preventDefault();
-});
+    $('.open').on('click', function(){
+      $overlay.addClass('state-show');
+      $modal.removeClass('state-leave').addClass('state-appear');
+    });
 
-$('.modal img').click(function(e) {
-  $('.overlay, .modal').removeClass('active');
-  e.preventDefault();
-});
+  });
+
 
 
