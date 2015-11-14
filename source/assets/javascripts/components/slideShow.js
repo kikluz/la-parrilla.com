f7d7005ebe1497b0bc331941be46282f9eb3856c
kikluz <savir81@hotmail.com>
2015-11-14 14:24:28 -0800

    add new code for image

--- source/assets/javascripts/components/slideShow.js
+++ source/assets/javascripts/components/slideShow.js
@@ -37,30 +37,7 @@ $('.prev').on('click', function(){
   });
 }*/
 
-// this for gallery demo
-
-$(document).on('ready', function(){
-    $modal = $('.modal-frame');
-    $overlay = $('.modal-overlay');
-
-    // Need this to clear out the keyframe classes so they dont clash with each other between ener/leave. Cheers. 
-    $modal.bind('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e){
-      if($modal.hasClass('state-leave')) {
-        $modal.removeClass('state-leave');
-      }
-    });
-
-    $('.close').on('click', function(){
-      $overlay.removeClass('state-show');
-      $modal.removeClass('state-appear').addClass('state-leave');
-    });
-
-    $('.open').on('click', function(){
-      $overlay.addClass('state-show');
-      $modal.removeClass('state-leave').addClass('state-appear');
-    });
-
-  });
+// multiples modals for menu images
 
 $('.open-modal').click(function() {
   var el = $(this)[0],
