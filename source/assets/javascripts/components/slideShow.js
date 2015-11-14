6ef46cc4b6b5d8faa6afa5db2d3b327d77f9eb16
kikluz <savir81@hotmail.com>
2015-11-14 14:14:03 -0800

    add new code for modals multiples

--- source/assets/javascripts/components/slideShow.js
+++ source/assets/javascripts/components/slideShow.js
@@ -62,5 +62,20 @@ $(document).on('ready', function(){
 
   });
 
+$('.open-modal').click(function() {
+  var el = $(this)[0],
+      classes = el.className.split(/\s+/);
+  for(var i = 0; i < classes.length; i++) {
+    if(classes[i].match(/modal-/)) {
+      var modalClass = classes[i];
+      $('.modal.' + modalClass).fadeIn('fast');
+    }
+  }
+});
+
+$('.close-modal').click(function() {
+  $(this).closest('.modal').fadeOut('fast');
+});
+
 
 
