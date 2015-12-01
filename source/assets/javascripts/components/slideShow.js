b91a7371a1b4947666ab3b185ac538a8a85fb973
kikluz <savir81@hotmail.com>
2015-11-30 16:52:12 -0800

    add new code

--- source/assets/javascripts/components/slideShow.js
+++ source/assets/javascripts/components/slideShow.js
@@ -41,18 +41,18 @@ $('.prev').on('click', function(){
 //demo for gallery page
 
  // portfolio
- $('.gallery ul li a').click(function() {
+ $('.gallery .demo-gallery div a').click(function() {
      var itemID = $(this).attr('href');
-     $('.gallery ul').addClass('item_open');
+     $('.gallery .demo-gallery').addClass('item_open');
      $(itemID).addClass('item_open');
      return false;
  });
  $('.close').click(function() {
-     $('.port, .gallery ul').removeClass('item_open');
+     $('.port, .gallery .demo-gallery').removeClass('item_open');
      return false;
  });
 
- $(".gallery ul li a").click(function() {
+ $(".gallery .demo-gallery div").click(function() {
      $('html, body').animate({
          scrollTop: parseInt($("#top").offset().top)
      }, 400);
